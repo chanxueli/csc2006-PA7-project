b433bd736df881f740b0dde0646d65b3076cf448
chanxueli <2717950C@student.gla.ac.uk>
2023-03-05 15:55:48 +0800

    modify mqtt trilateration integration

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from easy_trilateration.model import *  
 from easy_trilateration.least_squares import *
 from easy_trilateration.graph import *  
-import random
 import paho.mqtt.client as mqtt
-import paho.mqtt.publish as publish
 import time
+# import random
+# import paho.mqtt.publish as publish
+
+recv1 = 0
+recv2 = 0
+recv3 = 0
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -17,7 +21,16 @@ def on_connect(client, userdata, flags, rc):
 def on_subscribe(client, userdata, flags, rc):
     print("Subscribed")
 
-def on_message(mqttc, obj, msg):
+def on_message(client, userdata, msg):
+    if (msg.topic == "Receiver/1"):
+        recv1 = msg.payload.decode()
+        print("recv1: ", recv1)
+    elif (msg.topic == "Receiver/2"):
+        recv2 = msg.payload.decode()
+        print("recv2: ", recv2)
+    elif (msg.topic == "Receiver/3"):
+        recv3 = msg.payload.decode()
+        print("recv3: ", recv3)
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 def calc(x, y, z):
@@ -29,13 +42,13 @@ def calc(x, y, z):
     return result
 
 host = "1659bd2762a749d79c6f9453c56635d3.s2.eu.hivemq.cloud" # broker
-port = 8883
+port = 8883 # broker port
 username = "test1"
 password = "testtest"
 client_id = "lkju2aehdglkuahflgiohk" # unique client id string used to connect to broker
 topic = "receviver/1"
 
-client = mqtt.Client(client_id=client_id)
+client = mqtt.Client(client_id=client_id) # client object / constructor
 client.username_pw_set(username=username, password=password) # add inside client object
 client.tls_set()
 
@@ -47,8 +60,9 @@ client.connect(host, port=port,)
 client.subscribe("receiver/#", qos=0)
 client.loop_start()
 
-while True:
-    time.sleep(3)
+while (recv1 != 0 and recv2 != 0 and recv3 != 0):
+    calc(recv1, recv2, recv3)
+    time.sleep(5)
 
 # arrRes = []
 
